959c58a7875fcc77532ff08df5f390a5905fec9c
linzixiong <lzxil00@gmail.com>
2019-09-21 20:00:11 +0800

    添加dockerfile用于构建docker镜像

--- vue-project/config/index.js
+++ vue-project/config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/org_manage',
+    assetsPublicPath: '/org_manage/',
     proxyTable: {
       '/api/': {
         target: 'http://127.0.0.1:8000'
