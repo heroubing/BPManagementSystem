57c2fa6f28a9206432ffcbb55bdefd53411b2c12
linzixiong <lzxil00@gmail.com>
2020-05-01 15:08:46 +0800

    投资管理功能修改

--- staff_admin/vue-project/src/components/Investment/Edit.vue
+++ staff_admin/vue-project/src/components/Investment/Edit.vue
@@ -105,7 +105,7 @@ export default {
   methods: {
     // 用户查询
     queryInputList (searchKey, cb, key) {
-      if (!searchKey) {
+      if (!searchKey && key === 'user') {
         let result = []
         cb(result)
         return
